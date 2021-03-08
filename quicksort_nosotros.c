a2e22afd8cd8a4b76ec795e9c813fc7cac988ba4
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2021-03-08 01:01:17 -0300

    Add files via upload

--- quicksort_nosotros.c
+++ quicksort_nosotros.c
@@ -6,29 +6,35 @@ void Quicksort(int elems[], int size);
 
 
 int main(){
-    int lst[] = {8, 7, 6, 1, 0, 9, 2};
+    //int lst[] = {8, 7, 6, 1, 0, 9, 2}; //Original test list
+    int lst[] = {84, 58, 22, 37, 30, 49, 79, 41, 60, 83, 65, 95, 73, 33, 87, 62, 46, 85, 28, 16};
     int size = sizeof(lst) / sizeof(lst[0]);
-   
+
+    printf("Unsorted: ");
     for (int i = 0; i < size; i++){
         printf("%d ", lst[i]);
     }
 
     Quicksort(lst, size);
-    
+
     printf("\n-----------\n");
 
+    printf("Sorted: ");
     for (int i = 0; i < size; i++){
         printf("%d ", lst[i]);
     }
+
+    printf("\n\n");
 
     return 0;
 }
 
 
 void Quicksort(int elems[], int size){
-    int left, right, pivot, aux;
+    int left, right, pivot, minusSize, aux;
+    minusSize = size - 1;
 
-    pivot = size - 1;
+    pivot = minusSize;
 
     if (size == 1){
         return;
@@ -40,20 +46,18 @@ void Quicksort(int elems[], int size){
             break;
         }
     }
-    
+
     right = left + 1;
 
     while(1){
         if (elems[right] < elems[left] && elems[right] < elems[pivot]){
             aux = elems[left];
-            
+
             elems[left] = elems[right];
             elems[right] = aux;
-            
+
             left++;
         }
-    
-        right++;
 
         if (right == pivot){
             aux = elems[left];
@@ -62,23 +66,33 @@ void Quicksort(int elems[], int size){
 
             break;
         }
+
+        right++;
     }
 
-    int leftArray[left];
+    if (left > 0){
+        int leftArray[left];
 
-    for(int i = 0; i < left; i++){
-        leftArray[i] = elems[i];
-        printf("Left: %d\n", leftArray[i]);
+        for (int i = 0; i < left; i++){
+            leftArray[i] = elems[i];
+        }
+
+        Quicksort(leftArray, left);
+
+        for (int i = 0; i < left; i++){
+            elems[i] = leftArray[i];
+        }
     }
-
-    Quicksort(leftArray, left + 1);
 
     int rightArray[size - left - 1];
 
-    for(int i = 0; i < size - left - 1; i++){
-        rightArray[i] = elems[size - left + i];
+    for (int i = 0; i < size - left - 1; i++){
+        rightArray[i] = elems[left + i + 1];
     }
 
     Quicksort(rightArray, size - left - 1);
 
+    for (int i = 0; i < size - left - 1; i++){
+        elems[left + i + 1] = rightArray[i];
+    }
 }
